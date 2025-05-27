14768902340bdbd90d44a88ba4c85d0fb41e4087
dvub <dvubdevs@gmail.com>
2025-05-27 00:43:09 -0700

    create files

---
+++ README.md
@@ -0,0 +1,13 @@
+# Convolution
+
+## Building
+
+After installing [Rust](https://rustup.rs/), you can compile Convolution as follows:
+
+```shell
+cargo xtask bundle convolution_plug --release
+```
+
+SHIT
+
+`Get-Content  "C:\Users\range\AppData\Local\Bitwig Studio\engine.log" -wait`
---
+++ src/lib.rs
@@ -0,0 +1,290 @@
+use fft_convolver::FFTConvolver;
+use nih_plug::prelude::*;
+use std::fs::File;
+use std::path::Path;
+use std::sync::Arc;
+use symphonia::core::audio::SampleBuffer;
+
+use symphonia::core::codecs::{DecoderOptions, CODEC_TYPE_NULL};
+use symphonia::core::errors::Error;
+use symphonia::core::formats::FormatOptions;
+use symphonia::core::io::MediaSourceStream;
+use symphonia::core::meta::MetadataOptions;
+use symphonia::core::probe::Hint;
+
+const CONVOLVER_BLOCK_SIZE: usize = 1000;
+
+// This is a shortened version of the gain example with most comments removed, check out
+// https://github.com/robbert-vdh/nih-plug/blob/master/plugins/examples/gain/src/lib.rs to get
+// started
+
+struct ConvolutionPlug {
+    scratch: Vec<f32>,
+
+    // TODO: not sure if this needs to be here
+    impulse_response: Vec<f32>,
+
+    convolver: FFTConvolver<f32>,
+
+    params: Arc<ConvolutionPlugParams>,
+}
+
+#[derive(Params)]
+struct ConvolutionPlugParams {
+    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
+    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
+    /// parameters are exposed to the host in the same order they were defined. In this case, this
+    /// gain parameter is stored as linear gain while the values are displayed in decibels.
+    #[id = "gain"]
+    pub gain: FloatParam,
+}
+
+impl Default for ConvolutionPlug {
+    fn default() -> Self {
+        Self {
+            impulse_response: Vec::new(),
+            convolver: FFTConvolver::default(),
+
+            scratch: Vec::new(),
+            params: Arc::new(ConvolutionPlugParams::default()),
+        }
+    }
+}
+
+impl Default for ConvolutionPlugParams {
+    fn default() -> Self {
+        Self {
+            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
+            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
+            // as decibels is easier to work with, but requires a conversion for every sample.
+            gain: FloatParam::new(
+                "Gain",
+                util::db_to_gain(0.0),
+                FloatRange::Skewed {
+                    min: util::db_to_gain(-30.0),
+                    max: util::db_to_gain(30.0),
+                    // This makes the range appear as if it was linear when displaying the values as
+                    // decibels
+                    factor: FloatRange::gain_skew_factor(-30.0, 30.0),
+                },
+            )
+            // Because the gain parameter is stored as linear gain instead of storing the value as
+            // decibels, we need logarithmic smoothing
+            .with_smoother(SmoothingStyle::Logarithmic(50.0))
+            .with_unit(" dB")
+            // There are many predefined formatters we can use here. If the gain was stored as
+            // decibels instead of as a linear gain value, we could have also used the
+            // `.with_step_size(0.1)` function to get internal rounding.
+            .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
+            .with_string_to_value(formatters::s2v_f32_gain_to_db()),
+        }
+    }
+}
+
+impl Plugin for ConvolutionPlug {
+    const NAME: &'static str = "Convolution";
+    const VENDOR: &'static str = "dvub";
+    const URL: &'static str = env!("CARGO_PKG_HOMEPAGE");
+    const EMAIL: &'static str = "todo";
+
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+    // The first audio IO layout is used as the default. The other layouts may be selected either
+    // explicitly or automatically by the host or the user depending on the plugin API/backend.
+    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[AudioIOLayout {
+        main_input_channels: NonZeroU32::new(2),
+        main_output_channels: NonZeroU32::new(2),
+
+        aux_input_ports: &[],
+        aux_output_ports: &[],
+
+        // Individual ports and the layout as a whole can be named here. By default these names
+        // are generated as needed. This layout will be called 'Stereo', while a layout with
+        // only one input and output channel would be called 'Mono'.
+        names: PortNames::const_default(),
+    }];
+
+    const MIDI_INPUT: MidiConfig = MidiConfig::None;
+    const MIDI_OUTPUT: MidiConfig = MidiConfig::None;
+
+    const SAMPLE_ACCURATE_AUTOMATION: bool = true;
+
+    // If the plugin can send or receive SysEx messages, it can define a type to wrap around those
+    // messages here. The type implements the `SysExMessage` trait, which allows conversion to and
+    // from plain byte buffers.
+    type SysExMessage = ();
+    // More advanced plugins can use this to run expensive background tasks. See the field's
+    // documentation for more information. `()` means that the plugin does not have any background
+    // tasks.
+    type BackgroundTask = ();
+
+    fn params(&self) -> Arc<dyn Params> {
+        self.params.clone()
+    }
+
+    fn initialize(
+        &mut self,
+        _audio_io_layout: &AudioIOLayout,
+        buffer_config: &BufferConfig,
+        _context: &mut impl InitContext<Self>,
+    ) -> bool {
+        let max_buf_len = buffer_config.max_buffer_size;
+        self.scratch = Vec::with_capacity(max_buf_len as usize);
+
+        self.impulse_response = get_samples();
+
+        self.convolver
+            .init(CONVOLVER_BLOCK_SIZE, &self.impulse_response)
+            .unwrap();
+
+        nih_log!("Initialized Convolution");
+        // Resize buffers and perform other potentially expensive initialization operations here.
+        // The `reset()` function is always called right after this function. You can remove this
+        // function if you do not need it.
+        true
+    }
+
+    fn reset(&mut self) {
+        // Reset buffers and envelopes here. This can be called from the audio thread and may not
+        // allocate. You can remove this function if you do not need it.
+    }
+
+    fn process(
+        &mut self,
+        buffer: &mut Buffer,
+        _aux: &mut AuxiliaryBuffers,
+        _context: &mut impl ProcessContext<Self>,
+    ) -> ProcessStatus {
+        for channel in buffer.as_slice() {
+            let scratch = &mut self.scratch[..channel.len()];
+            scratch.copy_from_slice(channel);
+
+            self.convolver.process(scratch, channel).unwrap();
+        }
+
+        ProcessStatus::Normal
+    }
+}
+
+impl ClapPlugin for ConvolutionPlug {
+    const CLAP_ID: &'static str = "com.your-domain.convolution-plug";
+    const CLAP_DESCRIPTION: Option<&'static str> = Some("todo");
+    const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
+    const CLAP_SUPPORT_URL: Option<&'static str> = None;
+
+    // Don't forget to change these features
+    const CLAP_FEATURES: &'static [ClapFeature] = &[ClapFeature::AudioEffect, ClapFeature::Stereo];
+}
+
+impl Vst3Plugin for ConvolutionPlug {
+    const VST3_CLASS_ID: [u8; 16] = *b"Exactly16Chars!!";
+
+    // And also don't forget to change these categories
+    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
+        &[Vst3SubCategory::Fx, Vst3SubCategory::Dynamics];
+}
+
+fn get_samples() -> Vec<f32> {
+    println!("hello");
+    let file = File::open(Path::new(
+        "D:\\projects\\rust\\convolution_plug\\vsmall.wav",
+    ))
+    .unwrap();
+
+    // why does it have to be boxed?
+    let mss = MediaSourceStream::new(Box::new(file), Default::default());
+
+    // Create a hint to help the format registry guess what format reader is appropriate. In this
+    // example we'll leave it empty.
+    let hint = Hint::new();
+
+    // Use the default options when reading and decoding.
+    let format_opts: FormatOptions = Default::default();
+    let metadata_opts: MetadataOptions = Default::default();
+    let decoder_opts: DecoderOptions = Default::default();
+
+    // Probe the media source stream for a format.
+    let probed = symphonia::default::get_probe()
+        .format(&hint, mss, &format_opts, &metadata_opts)
+        .unwrap();
+
+    // Get the format reader yielded by the probe operation.
+    let mut format = probed.format;
+
+    // Get the default track.
+    let track = format.default_track().unwrap();
+
+    // Create a decoder for the track.
+    let mut decoder = symphonia::default::get_codecs()
+        .make(&track.codec_params, &decoder_opts)
+        .unwrap();
+
+    // Store the track identifier, we'll use it to filter packets.
+    let track_id = track.id;
+
+    let mut sample_count = 0;
+    let mut sample_buf = None;
+
+    let mut out = Vec::new();
+    loop {
+        // https://github.com/pdeljanov/Symphonia/issues/62
+        // Get the next packet from the format reader.
+        let packet = match format.next_packet() {
+            Ok(packet_ok) => packet_ok,
+            Err(Error::IoError(_)) => {
+                println!("complete!");
+                break;
+            }
+            Err(packet_err) => panic!("{:?}", packet_err),
+        };
+
+        // If the packet does not belong to the selected track, skip it.
+        if packet.track_id() != track_id {
+            continue;
+        }
+
+        // Decode the packet into audio samples, ignoring any decode errors.
+        match decoder.decode(&packet) {
+            Ok(audio_buf) => {
+                // The decoded audio samples may now be accessed via the audio buffer if per-channel
+                // slices of samples in their native decoded format is desired. Use-cases where
+                // the samples need to be accessed in an interleaved order or converted into
+                // another sample format, or a byte buffer is required, are covered by copying the
+                // audio buffer into a sample buffer or raw sample buffer, respectively. In the
+                // example below, we will copy the audio buffer into a sample buffer in an
+                // interleaved order while also converting to a f32 sample format.
+
+                // If this is the *first* decoded packet, create a sample buffer matching the
+                // decoded audio buffer format.
+                if sample_buf.is_none() {
+                    // Get the audio buffer specification.
+                    let spec = *audio_buf.spec();
+
+                    // Get the capacity of the decoded buffer. Note: This is capacity, not length!
+                    let duration = audio_buf.capacity() as u64;
+
+                    // Create the f32 sample buffer.
+                    sample_buf = Some(SampleBuffer::<f32>::new(duration, spec));
+                }
+
+                // Copy the decoded audio buffer into the sample buffer in an interleaved format.
+                if let Some(buf) = &mut sample_buf {
+                    buf.copy_interleaved_ref(audio_buf);
+
+                    // The samples may now be access via the `samples()` function.
+                    sample_count += buf.samples().len();
+                    nih_log!("Decoded {} samples", sample_count);
+                    for s in buf.samples() {
+                        out.push(*s);
+                    }
+                }
+            }
+            Err(Error::DecodeError(_)) => (),
+            Err(_) => break,
+        }
+    }
+    out
+}
+
+nih_export_clap!(ConvolutionPlug);
+nih_export_vst3!(ConvolutionPlug);
