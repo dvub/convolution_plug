0174fece9b3beb1a15e636d59932614b86d5dd04
dvub <dvubdevs@gmail.com>
2025-06-07 16:18:12 -0700

    poo

--- src/dsp/mod.rs
+++ src/dsp/mod.rs
@@ -16,13 +16,13 @@ pub fn build_graph(p: &Arc<PluginParams>, ir_samples: &[f32]) -> Box<dyn AudioUn
         * ((multipass::<U1>() | lp_cutoff::<U1>(p) | lp_q::<U1>(p)) >> lowpass()))
         & ((1.0 - lp_enabled(p)) * multipass::<U1>());
 
-    let mut convolver_slot = fundsp::hacker32::Slot::new(Box::new(convolver(ir_samples)));
+    let mut convolver_slot = Slot::new(Box::new(convolver(ir_samples)));
 
     let slot_front = convolver_slot.0;
-    let slot_back = convolver_slot.1;
+    let slot_back = Box::new(convolver_slot.1);
     let mut net = Net::new(1, 1);
 
-    let mono_wet = convolver_slot >> lp;
+    let mono_wet = slot_back >> lp;
 
     let wet = mono_wet * dry_wet(p);
     let dry = pass() * (1.0 - dry_wet(p));
