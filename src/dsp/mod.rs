60a2446850f3d67bf360f2d634302bf090d8a0c0
dvub <dvubdevs@gmail.com>
2025-06-17 18:23:08 -0700

    actually fixed dsp graph

--- src/dsp/mod.rs
+++ src/dsp/mod.rs
@@ -7,25 +7,9 @@ use std::sync::Arc;
 use param_nodes::*;
 
 use crate::params::PluginParams;
+
 // yep this is the big thing
 pub fn build_graph(p: &Arc<PluginParams>) -> (Box<dyn AudioUnit>, Slot) {
-    // TODO: rewrite the entire EQ section to be not bad
-    let lp = (lp_enabled(p)
-        * ((multipass::<U2>() | lp_freq::<U2>(p) | lp_q::<U2>(p)) >> (lowpass() | lowpass())))
-        & ((1.0 - lp_enabled(p)) * multipass::<U2>());
-
-    let hp = (hp_enabled(p)
-        * ((multipass::<U2>() | hp_freq::<U2>(p) | hp_q::<U2>(p)) >> (highpass() | highpass())))
-        & ((1.0 - hp_enabled(p)) * multipass::<U2>());
-
-    let bl = (bell_enabled(p)
-        * ((multipass::<U2>()
-            | bell_freq::<U2>(p)
-            | param_nodes::bell_q::<U2>(p)
-            | bell_gain::<U2>(p))
-            >> (bell() | bell())))
-        & ((1.0 - bell_enabled(p)) * multipass::<U2>());
-
     // no IR is loaded by default.
     // we don't even have to convolve by an empty IR, e.g. [1.0, 0.0, 0.0 ... ],
     // we can simply pass the signal straight through for the best performance
@@ -35,15 +19,45 @@ pub fn build_graph(p: &Arc<PluginParams>) -> (Box<dyn AudioUnit>, Slot) {
     let slot_back = convolver_slot.1;
 
     let convolver = unit::<U2, U2>(Box::new(slot_back));
-
-    let eq_wet = convolver >> lp >> bl >> hp;
+    let eq_wet = convolver >> switched_lowpass(p) >> switched_bell(p) >> switched_highpass(p);
 
     let wet = eq_wet * dry_wet(p);
     let dry = multipass::<U2>() * (1.0 - dry_wet(p));
-
     let mixed = wet & dry;
 
     let graph = mixed * gain(p);
 
     (Box::new(graph), slot_front)
+}
+
+fn lp_with_params(p: &Arc<PluginParams>) -> An<impl AudioNode<Inputs = U1, Outputs = U1>> {
+    (pass() | lp_freq::<U1>(p) | lp_q::<U1>(p)) >> lowpass()
+}
+
+fn hp_with_params(p: &Arc<PluginParams>) -> An<impl AudioNode<Inputs = U1, Outputs = U1>> {
+    (pass() | hp_freq::<U1>(p) | hp_q::<U1>(p)) >> highpass()
+}
+
+fn bell_with_params(p: &Arc<PluginParams>) -> An<impl AudioNode<Inputs = U1, Outputs = U1>> {
+    (pass() | bell_freq::<U1>(p) | param_nodes::bell_q::<U1>(p) | bell_gain::<U1>(p)) >> bell()
+}
+
+// ...
+
+fn switched_bell(p: &Arc<PluginParams>) -> An<impl AudioNode<Inputs = U2, Outputs = U2>> {
+    let stereo_bell = bell_with_params(p) | bell_with_params(p);
+
+    (bell_enabled(p) * stereo_bell) & ((1.0 - bell_enabled(p)) * multipass::<U2>())
+}
+
+fn switched_lowpass(p: &Arc<PluginParams>) -> An<impl AudioNode<Inputs = U2, Outputs = U2>> {
+    let stereo_lowpass = lp_with_params(p) | lp_with_params(p);
+
+    (lp_enabled(p) * stereo_lowpass) & ((1.0 - lp_enabled(p)) * multipass::<U2>())
+}
+
+fn switched_highpass(p: &Arc<PluginParams>) -> An<impl AudioNode<Inputs = U2, Outputs = U2>> {
+    let stereo_highpass = hp_with_params(p) | hp_with_params(p);
+
+    (hp_enabled(p) * stereo_highpass) & ((1.0 - hp_enabled(p)) * multipass::<U2>())
 }
