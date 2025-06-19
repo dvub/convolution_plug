825f6425b9b354112375411ee713d947186e7134
dvub <dvubdevs@gmail.com>
2025-06-19 16:27:42 -0700

    minor profiling changes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::{fs::File, hint::black_box, sync::Arc};
+use std::sync::Arc;
 
-use convolution_plug::{
-    dsp::{build_graph, nodes::gain},
-    params::PluginParams,
-};
+use convolution_plug::{dsp::nodes::gain, params::PluginParams};
 use fundsp::hacker32::*;
 
 fn main() {
@@ -14,7 +11,6 @@ fn main() {
     let mut graph = noise() * gain(&params);
 
     let wave = Wave::render(44100.0, 1.0, &mut graph);
-    wave.save_wav16("test.wav").expect("Could not save wave.");
 
     // println!("Done rendering.");
 }
