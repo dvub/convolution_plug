11784f68f8eeed706584cee33c9fb0275d15e106
dvub <dvubdevs@gmail.com>
2025-07-04 21:16:22 -0700

    wow i am really bad at this

--- convolution-gui/app/page.tsx
+++ convolution-gui/app/page.tsx
@@ -1,88 +1,107 @@
-"use client";
+'use client';
 
-import { MessageBus, MessageBusContext } from "@/contexts/MessageBusContext";
-import { useEventDispatcher } from "@/hooks/useEventDispatcher";
-import { useEffect, useState } from "react";
+import { MessageBus, MessageBusContext } from '@/contexts/MessageBusContext';
+import { useEventDispatcher } from '@/hooks/useEventDispatcher';
+import { useEffect, useState } from 'react';
 import {
-  GlobalParameters,
-  GlobalParametersContext,
-} from "@/contexts/GlobalParamsContext";
+	GlobalParameters,
+	GlobalParametersContext,
+} from '@/contexts/GlobalParamsContext';
 
-import { Message } from "@/bindings/Message";
+import { Message } from '@/bindings/Message';
 
-import { sendToPlugin } from "@/lib";
-import { Knob } from "@/components/knobs/Knob";
-import { dbToGain, gainToDb, NormalisableRange } from "@/lib/utils";
+import { sendToPlugin } from '@/lib';
+import { Knob } from '@/components/knobs/Knob';
+import { dbToGain, gainToDb, NormalisableRange } from '@/lib/utils';
 
-import { FileInput } from "@/components/fileInput";
+import { IrLoader } from '@/components/IRLoader';
+import { GearIcon } from '@radix-ui/react-icons';
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
+	const [isLoading, setIsLoading] = useState(true);
 
-  const [messageBus] = useState(new MessageBus());
-  const [parameters, setParameters] = useState<GlobalParameters>({
-    gain: 0,
-    dry_wet: 0,
+	const [messageBus] = useState(new MessageBus());
+	const [parameters, setParameters] = useState<GlobalParameters>({
+		gain: 0,
+		dry_wet: 0,
 
-    lowpass_enabled: 0,
-    lowpass_freq: 0,
-    lowpass_q: 0,
+		lowpass_enabled: 0,
+		lowpass_freq: 0,
+		lowpass_q: 0,
 
-    bell_enabled: 0,
-    bell_freq: 0,
-    bell_q: 0,
-    bell_gain: 0,
+		bell_enabled: 0,
+		bell_freq: 0,
+		bell_q: 0,
+		bell_gain: 0,
 
-    highpass_enabled: 0,
-    highpass_freq: 0,
-    highpass_q: 0,
-  });
-  useEventDispatcher(messageBus);
-  useEffect(() => {
-    sendToPlugin({ type: "init" });
-    const handlePluginMessage = (event: Message) => {
-      if (isLoading) {
-        setIsLoading(false);
-      }
+		highpass_enabled: 0,
+		highpass_freq: 0,
+		highpass_q: 0,
+	});
+	useEventDispatcher(messageBus);
+	useEffect(() => {
+		sendToPlugin({ type: 'init' });
+		const handlePluginMessage = (event: Message) => {
+			if (isLoading) {
+				setIsLoading(false);
+			}
 
-      if (event.type === "parameterUpdate") {
-        setParameters((prevState) => {
-          return {
-            ...prevState,
-            [event.data.parameterId]: event.data.value,
-          };
-        });
-      }
-    };
-    const unsubscribe = messageBus.subscribe(handlePluginMessage);
-    return () => {
-      unsubscribe();
-    };
-  }, [messageBus]);
+			if (event.type === 'parameterUpdate') {
+				setParameters((prevState) => {
+					return {
+						...prevState,
+						[event.data.parameterId]: event.data.value,
+					};
+				});
+			}
+		};
+		const unsubscribe = messageBus.subscribe(handlePluginMessage);
+		return () => {
+			unsubscribe();
+		};
+	}, [messageBus]);
 
-  return (
-    <MessageBusContext.Provider value={messageBus}>
-      <GlobalParametersContext.Provider value={{ parameters, setParameters }}>
-        <FileInput />
-        <Knob
-          parameter="gain"
-          label={"Gain"}
-          size={50}
-          //
-          cosmeticDefaultValue={dbToGain(0)}
-          cosmeticRange={
-            new NormalisableRange(dbToGain(-30), dbToGain(30), dbToGain(0))
-          }
-          valueRawDisplayFn={(x) => {
-            let g = gainToDb(x).toFixed(2);
-            // TODO: make this not be really scuffed
-            if (g === "-0.00") {
-              g = "0.00";
-            }
-            return `${g} dB`;
-          }}
-        ></Knob>
-      </GlobalParametersContext.Provider>
-    </MessageBusContext.Provider>
-  );
+	return (
+		<MessageBusContext.Provider value={messageBus}>
+			<GlobalParametersContext.Provider
+				value={{ parameters, setParameters }}
+			>
+				<div className='m-1 flex-col h-[100%]'>
+					<div className='flex justify-between gap-2 items-center'>
+						<h1>CONVOLUTION</h1>
+						<div className='w-full text-right'>
+							<h1>dvub</h1>
+						</div>
+
+						<GearIcon />
+					</div>
+					<IrLoader />
+
+					{/* <Knob
+						parameter='gain'
+						label={'Gain'}
+						size={50}
+						//
+						cosmeticDefaultValue={dbToGain(0)}
+						cosmeticRange={
+							new NormalisableRange(
+								dbToGain(-30),
+								dbToGain(30),
+								dbToGain(0)
+							)
+						}
+						valueRawDisplayFn={(x) => {
+							let g = gainToDb(x).toFixed(2);
+							// TODO: make this not be really scuffed
+							if (g === '-0.00') {
+								g = '0.00';
+							}
+							return `${g} dB`;
+						}}
+					/>*/}
+					<div className='w-[60%] h-10 bg-red-500'></div>
+				</div>
+			</GlobalParametersContext.Provider>
+		</MessageBusContext.Provider>
+	);
 }
---
+++ convolution-gui/components/IRLoader.tsx
@@ -0,0 +1,108 @@
+import { Message } from '@/bindings/Message';
+import { MessageBusContext } from '@/contexts/MessageBusContext';
+import { sendToPlugin } from '@/lib';
+import { GearIcon, UploadIcon, ValueNoneIcon } from '@radix-ui/react-icons';
+
+import { ChangeEvent, useContext, useEffect, useRef, useState } from 'react';
+import WaveSurfer from 'wavesurfer.js';
+
+export function IrLoader() {
+	const waveSurferRef = useRef<WaveSurfer | null>(null);
+	const containerRef = useRef(null);
+
+	const messageBus = useContext(MessageBusContext)!;
+	const [fileName, setFileName] = useState('...');
+
+	useEffect(() => {
+		const waveSurfer = WaveSurfer.create({
+			container: containerRef.current!,
+			height: 200,
+			waveColor: 'white',
+			normalize: true,
+		});
+
+		waveSurfer.on('init', () => {
+			waveSurferRef.current = waveSurfer;
+		});
+
+		const handlePluginMessage = (event: Message) => {
+			if (event.type === 'irUpdate') {
+				const blob = new Blob([new Uint8Array(event.data.rawBytes)], {
+					type: 'wav',
+				});
+				waveSurfer.loadBlob(blob);
+				setFileName(event.data.name);
+			}
+		};
+
+		const unsubscribe = messageBus.subscribe(handlePluginMessage);
+		return () => {
+			unsubscribe();
+			waveSurfer.destroy();
+		};
+	}, [messageBus]);
+
+	function onFileChange(event: ChangeEvent<HTMLInputElement>) {
+		if (!event.target.files || event.target.files.length === 0) {
+			return;
+		}
+		const fileName = event.target.files[0].name;
+
+		// first, configure the reader & behavior
+		const reader = new FileReader();
+		reader.onload = () => {
+			const arrayBuffer = reader.result as ArrayBuffer;
+			const bytes = new Uint8Array(arrayBuffer);
+
+			sendToPlugin({
+				type: 'irUpdate',
+				data: {
+					name: fileName,
+					rawBytes: [...bytes],
+				},
+			});
+		};
+		// now pass the file into the reader
+
+		// we can also be sure that there will be a file here (i think)
+		const input = event.target.files[0];
+		reader.readAsArrayBuffer(input);
+
+		// finally, visualization
+		waveSurferRef.current?.loadBlob(input);
+
+		setFileName(fileName);
+	}
+	return (
+		<div className='border-1 rounded-sm w-[50%]'>
+			<div>
+				<div ref={containerRef} className='w-full' />
+			</div>
+
+			<div className='flex items-center justify-between gap-2 border-t-1 text-md'>
+				<input
+					id='inp'
+					type='file'
+					onChange={onFileChange}
+					className='hidden'
+				/>
+				<label
+					htmlFor='inp'
+					className='p-1 hover:cursor-pointer border-r-1 rounded-r-sm'
+				>
+					<UploadIcon />
+				</label>
+				<div className='w-full'>
+					<h1>{fileName}</h1>
+				</div>
+
+				<button className='p-1 hover:cursor-pointer border-l-1 rounded-l-sm'>
+					<ValueNoneIcon />
+				</button>
+				<button className='p-1 hover:cursor-pointer border-l-1 rounded-l-sm'>
+					<GearIcon />
+				</button>
+			</div>
+		</div>
+	);
+}
