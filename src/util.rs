39ac8abd167efd49ead8aca476f04b7b6cc639be
dvub <dvubdevs@gmail.com>
2025-07-08 21:34:00 -0700

    improve file reading on backend

--- src/util.rs
+++ src/util.rs
@@ -1,17 +1,31 @@
+use hound::SampleFormat;
 use nih_plug::util::db_to_gain;
 
+// NOTE: right now this only handles mono
 pub fn decode_ir_samples(bytes: &[u8]) -> anyhow::Result<(Vec<f32>, f32)> {
     let mut reader = hound::WavReader::new(bytes)?;
 
-    let bit_depth = reader.spec().bits_per_sample as u32;
-    let sample_rate = reader.spec().sample_rate as f32;
+    let spec = reader.spec();
+    let sample_rate = spec.sample_rate as f32;
 
-    let max_amplitude = 2_i32.pow(bit_depth - 1) as f32;
+    let samples = match spec.sample_format {
+        // this format is fairly rare but we might encounter it,
+        // so we need to support it
+        SampleFormat::Float => reader
+            .samples::<f32>()
+            .map(|x| Ok(x?))
+            .collect::<anyhow::Result<Vec<f32>>>()?,
+        SampleFormat::Int => {
+            let bit_depth = spec.bits_per_sample as u32;
+            // 1 << (bit_depth - 1) might be correct
+            let max_amplitude = 2_i64.pow(bit_depth - 1) as f32;
 
-    let samples = reader
-        .samples::<i32>()
-        .map(|s| s.unwrap_or(0) as f32 / max_amplitude)
-        .collect();
+            reader
+                .samples::<i32>()
+                .map(|x| Ok(x? as f32 / max_amplitude))
+                .collect::<anyhow::Result<Vec<f32>>>()?
+        }
+    };
 
     Ok((samples, sample_rate))
 }
@@ -33,27 +47,54 @@ pub fn rms_normalize(input_signal: &mut [f32], desired_level_db: f32) {
 
 #[cfg(test)]
 mod tests {
-    use std::{
-        f32::consts::PI,
-        fs::{read, remove_file},
-    };
+    use std::{f32::consts::PI, fs::read, path};
 
     use float_cmp::approx_eq;
+    use tempdir::TempDir;
 
     use crate::util::decode_ir_samples;
 
     // test function
     // this writes a file AND returns an array of the samples
     // then the read function can be tested by comparing samples
-    fn write_test_file(name: &str, num_samples: usize) -> Vec<f32> {
+
+    fn write_test_file<P>(name: P, num_samples: usize) -> anyhow::Result<Vec<f32>>
+    where
+        P: AsRef<path::Path>,
+    {
         let spec = hound::WavSpec {
             channels: 1,
             sample_rate: 44100,
-            bits_per_sample: 16,
+            bits_per_sample: 32,
             sample_format: hound::SampleFormat::Int,
         };
 
         let len = num_samples;
+
+        let mut writer = hound::WavWriter::create(name, spec)?;
+        let mut samples = Vec::new();
+
+        for t in (0..len).map(|x| x as f32 / (len as f32)) {
+            let sample = (t * 440.0 * 2.0 * PI).sin();
+            samples.push(sample);
+            let amplitude = i32::MAX as f32;
+            writer.write_sample((sample * amplitude) as i32)?;
+        }
+        writer.finalize()?;
+
+        Ok(samples)
+    }
+    // TODO: it might be possible to refactor these functions to be more similar
+    fn write_test_file_f32<P>(name: P, len: usize) -> anyhow::Result<Vec<f32>>
+    where
+        P: AsRef<path::Path>,
+    {
+        let spec = hound::WavSpec {
+            channels: 1,
+            sample_rate: 44100,
+            bits_per_sample: 32,
+            sample_format: hound::SampleFormat::Float,
+        };
 
         let mut writer = hound::WavWriter::create(name, spec).unwrap();
         let mut samples = Vec::new();
@@ -61,31 +102,43 @@ mod tests {
         for t in (0..len).map(|x| x as f32 / (len as f32)) {
             let sample = (t * 440.0 * 2.0 * PI).sin();
             samples.push(sample);
-            let amplitude = i16::MAX as f32;
-            writer.write_sample((sample * amplitude) as i16).unwrap();
+            writer.write_sample(sample).unwrap();
         }
         writer.finalize().unwrap();
 
-        samples
+        Ok(samples)
     }
 
-    // TODO: make this test pass without manually checking
     #[test]
-    fn decode_samples() {
-        // TODO: use better name and proper temp directory
-        let file_name = "sine.wav";
+    fn decode_samples() -> anyhow::Result<()> {
+        let temp_dir = TempDir::new("wav_testing")?;
+        let file_name = temp_dir.path().join("test_sine.wav");
+        let samples = write_test_file(&file_name, 100)?;
 
-        let samples = write_test_file(file_name, 100);
-
-        let buf = read(file_name).unwrap();
-
-        // println!("{:?}", buf);
+        let buf = read(&file_name)?;
         let (other, _) = decode_ir_samples(&buf).unwrap();
 
-        // this might be horrible
-        remove_file(file_name).unwrap();
+        for (l, r) in samples.iter().zip(other) {
+            // TODO: find proper epsilon
+            assert!(approx_eq!(f32, *l, r, epsilon = 0.0000001))
+        }
 
-        assert_eq!(samples, other);
+        temp_dir.close()?;
+        Ok(())
+    }
+    #[test]
+    fn decode_samples_f32() -> anyhow::Result<()> {
+        let temp_dir = TempDir::new("wav_testing")?;
+        let file_name = temp_dir.path().join("test_sine.wav");
+        let original_samples = write_test_file_f32(&file_name, 100)?;
+
+        let buf = read(&file_name)?;
+        let (result_samples, _) = decode_ir_samples(&buf).unwrap();
+
+        assert_eq!(original_samples, result_samples);
+
+        temp_dir.close()?;
+        Ok(())
     }
 
     // TODO: make this stupid test pass
