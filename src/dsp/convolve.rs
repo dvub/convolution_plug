6d60b3dc8aa3f3d811362d6e7cadbf6a43d7dca1
dvub <dvubdevs@gmail.com>
2025-07-09 18:40:42 -0700

    fixed major performance issue

--- src/dsp/convolve.rs
+++ src/dsp/convolve.rs
@@ -15,17 +15,26 @@ impl AudioNode for Convolver {
 
     type Inputs = U1;
     type Outputs = U1;
-
+    /// performance is horrible, avoid calling tick() if possible
     fn tick(&mut self, input: &Frame<f32, Self::Inputs>) -> Frame<f32, Self::Outputs> {
         let mut output = [0.0];
         self.convolver.process(input, &mut output);
         Frame::from(output)
     }
+
+    // this allows block processing to occur, which is... better.
+    fn process(&mut self, _size: usize, input: &BufferRef, output: &mut BufferMut) {
+        self.convolver
+            .process(input.channel_f32(0), output.channel_f32_mut(0));
+    }
 }
+
+// seemed to perform the best?
+const BLOCK_SIZE: usize = 1024;
 
 // opcode
 pub fn convolver(samples: &[f32]) -> An<Convolver> {
-    let convolver = FFTConvolver::init(samples, 256, samples.len());
+    let convolver = FFTConvolver::init(samples, BLOCK_SIZE, samples.len());
 
     An(Convolver { convolver })
 }
